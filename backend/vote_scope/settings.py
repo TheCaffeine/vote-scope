144307a66a9d54955cb8133e7687b8bbae1cb8e4
TheCaffeine <85110314+TheCaffeine@users.noreply.github.com>
2022-01-11 17:41:43 +0300

    Enabled CORS to all = true

--- backend/vote_scope/settings.py
+++ backend/vote_scope/settings.py
@@ -39,6 +39,8 @@ REST_FRAMEWORK = {
  
 }
 
+CORS_ORIGIN_ALLOW_ALL = True
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -46,9 +48,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
