7d019d89bfe5234ff3bbced1643a6cc8d42641ce
TheCaffeine <85110314+TheCaffeine@users.noreply.github.com>
2022-01-11 07:59:41 +0300

    Update settings.py

--- backend/vote_scope/settings.py
+++ backend/vote_scope/settings.py
@@ -32,9 +32,9 @@ REST_FRAMEWORK = {
   [
     'rest_framework.authentication.SessionAuthentication',
     'rest_framework.authentication.BasicAuthentication'
- ],
-  'DEFAULT_PERMISSION_CLASSES': 
+[
     'rest_framework.permissions.AllowAny',
+]
  
 }
 
