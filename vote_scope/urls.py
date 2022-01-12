a00b025dcc17102ca436157d4ab7d0d320ca1ad4
monginadiana <diana.ondieki@student.moringa.com>
2022-01-12 22:05:34 +0300

    addition of the virtual environment

--- vote_scope/urls.py
+++ vote_scope/urls.py
@@ -1,7 +1,6 @@
-"""vote_scope URL Configuration
-
+"""hoodproject URL Configuration
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,9 +12,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib.auth import views 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from django_registration.backends.one_step.views import RegistrationView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include('voters.urls')),
+    path('accounts/register/', RegistrationView.as_view(success_url='/'),name='django_registration_register'),
+    path('accounts/', include('django_registration.backends.one_step.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('logout/', views.logout_then_login, name='logout'), 
+    
+    
+    
 ]
