a024ed94cdac4225fa81e5299ad3ed5f91550182
monginadiana <diana.ondieki@student.moringa.com>
2022-01-12 23:39:43 +0300

    addition of the views.py and the migrations

--- voters/views.py
+++ voters/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+
+@login_required(login_url="/accounts/login/")
+def index(request):
+    return render(request, 'all-votes/index.html')
