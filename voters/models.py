9aa24868df24f7f4ef3072a357e86a9fcbc7c988
monginadiana <diana.ondieki@student.moringa.com>
2022-01-13 08:18:27 +0300

    addition of the profile class

--- voters/models.py
+++ voters/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,null=True)
+    profile_photo = CloudinaryField('image')
+    bio = models.TextField(max_length=500,  null=True)
+    email = models.EmailField(null=True)
+    contact = models.CharField(max_length=50, blank=True, null=True)
+
+    def update(self):
+        self.save()
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    def create_profile(self):
+        self.save()
+
+    def update_profile(self):
+        self.update()
+
+    @classmethod
+    def get_profile_by_user(cls, user):
+        profile = cls.objects.filter(user=user)
+        return profile
+
+    def __str__(self):
+        return self.user.username
