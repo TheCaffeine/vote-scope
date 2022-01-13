5c41654ba85feaec92d6369832d462f4f1337b8d
monginadiana <diana.ondieki@student.moringa.com>
2022-01-13 10:49:57 +0300

    addition of the urls.py

--- voters/urls.py
+++ voters/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('FAQ/',views.FAQ ,name = 'FAQ'),
     path('accounts/profile/', views.index,name='index'),
     path('profile/',views.profile,name = 'profile'),
-    path('create_profile/',views.create_profile,name = 'create_profile'),
     path('update_profile/<int:id>',views.update_profile, name='update_profile'),
+    path('<str:username>/', views.profile, name='profile'),
+  
 ]
