feb0270cf43c86bfd2c2d4d09d95f3c3e8229bb2
monginadiana <diana.ondieki@student.moringa.com>
2022-01-13 10:51:15 +0300

    deleted the updateprofile and createforms in the forms.py

--- voters/forms.py
+++ voters/forms.py
@@ -3,13 +3,3 @@ from django.db.models import fields
 from django.forms import ModelForm
 from .models import *
 
-class UpdateProfileForm(forms.ModelForm):
-    class Meta:
-        model = Profile
-        fields = ['profile_photo','bio','contact']
-
-
-class ProfileForm(ModelForm):
-    class Meta:
-        model = Profile
-        exclude = ['user','email']
