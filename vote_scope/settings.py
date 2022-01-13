9ba67b8ead9bdbb3c8b5952b2bd50edd783134e5
monginadiana <diana.ondieki@student.moringa.com>
2022-01-13 10:51:44 +0300

    creation of the migrations

--- vote_scope/settings.py
+++ vote_scope/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from decouple import  config
+
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -38,7 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'voters',
-    'bootstrap4'
+    'bootstrap4',
+    'cloudinary',
 ]
 
 MIDDLEWARE = [
@@ -75,10 +82,26 @@ WSGI_APPLICATION = 'vote_scope.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+cloudinary.config( 
+  cloud_name = "mongina", 
+  api_key = "332865662615486", 
+  api_secret = "Mu5vzosaB_bVW-p8GaqQ0RHKtu4" 
+)
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': 'mongina',
+    'API_KEY': '332865662615486',
+    'API_SECRET': 'Mu5vzosaB_bVW-p8GaqQ0RHKtu4'
+}
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD':config('DB_PASSWORD'),
+        'HOST':config('DB_HOST'),
     }
 }
 
@@ -126,3 +149,6 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
     
 )
+
+DEFAULT_AUTO_FIELD='django.db.models.AutoField' 
+
